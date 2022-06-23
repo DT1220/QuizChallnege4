7b678dc0e1f117993f27e85268938c5e1dee013d
Daniel <danieltang@Daniels-MBP.lan>
2022-06-23 18:04:31 -0500

    initial Commit

---
+++ QuizChallenge4/script.js
@@ -0,0 +1,9 @@
+
+var question = [{
+    question: "How do you set a title in HTML?",
+    choiceA: "<head>",
+    choiceB: "<title>",
+    choiceC: "</title>",
+    choiceD: "title",
+    correctAnswer: "B"},
+]
